2e6c35f7a876456b1d9b57d879101ed06c4dd36a
Decors <decors1215@gmail.com>
2016-02-26 16:29:55 +0900

    Add completion

---
+++ completions/colors.fish
@@ -0,0 +1,4 @@
+complete -f -c colors -l help -s h -d "Show help"
+complete -f -c colors -l ansi -d "Display ANSI colors table (default option)"
+complete -f -c colors -l aixterm -s h -d "Display aixterm high intensity colors table"
+complete -f -c colors -l 256colors -s h -d "Display 256 colors table"
