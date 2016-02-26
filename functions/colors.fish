57a9e5962ac9d15611ca1620d7bee161351c2b97
Decors <decors1215@gmail.com>
2016-02-26 16:22:16 +0900

    Update
    Changes:
    * Add invalid option message
    * Replace escape code

--- functions/colors.fish
+++ functions/colors.fish
@@ -15,8 +15,10 @@ function colors
             echo '  --ansi       Display ANSI colors table (default option)'
             echo '  --aixterm    Display aixterm high intensity colors table'
             echo '  --256colors  Display 256 colors table'
-        case '*'
+        case ''
             colors::ansi
+        case '*'
+            echo "colors: invalid option $option"
     end
 end
 
@@ -24,9 +26,9 @@ function colors::ansi
     set text 'gYw'
 
     echo
-    echo -e -n "\033[1mANSI Standard Colors\033[0m :\n"
-    echo -e -n "  foreground: ESC[{\033[38;5;196m30-37\033[0m}m, bright foreground: ESC[1;{\033[38;5;196m30-37\033[0m}m\n"
-    echo -e -n "  background: ESC[{\033[38;5;196m40-47\033[0m}m\n"
+    echo -e -n "\e[1mANSI Standard Colors\e[0m :\n"
+    echo -e -n "  foreground: ESC[{\e[38;5;196m30-37\e[0m}m, bright foreground: ESC[1;{\e[38;5;196m30-37\e[0m}m\n"
+    echo -e -n "  background: ESC[{\e[38;5;196m40-47\e[0m}m\n"
 
     echo -e -n "\n     "
     for BG in '   ' '40m' '41m' '42m' '43m' '44m' '45m' '46m' '47m'
@@ -38,9 +40,9 @@ function colors::ansi
 
     for i in (seq (count $foregrounds))
         set FG  $foregrounds[$i]
-        printf " %5s \033[$FG  $text " $FG
+        printf " %5s \e[$FG  $text " $FG
         for BG in '40m' '41m' '42m' '43m' '44m' '45m' '46m' '47m';
-            printf " \033[$FG\033[$BG  $text  \033[0m";
+            printf " \e[$FG\e[$BG  $text  \e[0m";
         end
         echo
     end
@@ -50,34 +52,34 @@ end
 function colors::256colors
 
     echo
-    echo -e -n "\033[1m256 Colors\033[0m :\n"
-    echo -e -n "  foreground: ESC[38;5;{\033[38;5;196m0-255\033[0m}m\n"
-    echo -e -n "  background: ESC[48;5;{\033[38;5;196m0-255\033[0m}m\n"
+    echo -e -n "\e[1m256 Colors\e0m :\n"
+    echo -e -n "  foreground: ESC[38;5;{\e[38;5;196m0-255\e[0m}m\n"
+    echo -e -n "  background: ESC[48;5;{\e[38;5;196m0-255\e[0m}m\n"
 
     echo
-    echo -e " \033[1mStandard Colors\033[0m :"
+    echo -e " \e[1mStandard Colors\e[0m :"
     for c in (seq 0 15)
         set bg (printf "48;5;%dm" $c)
-        printf " \033[%s %3s \033[0m" $bg $c
+        printf " \e[%s %3s \e[0m" $bg $c
         [ "$c" = '7' ]; and echo
     end
     echo
 
     echo
-    echo -e " \033[1m216 Colors\033[0m :"
+    echo -e " \e[1m216 Colors\e[0m :"
     for v in (seq 16 12 231)
         for h in (seq $v 1 231)[1..12]
             set bg (printf "48;5;%dm" $h)
-            printf " \033[%s %3s \033[0m" $bg $h
+            printf " \e[%s %3s \e[0m" $bg $h
         end
         echo
     end
     echo
 
-    echo -e " \033[1mGrayscale Colors\033[0m :"
+    echo -e " \e[1mGrayscale Colors\e[0m :"
     for c in (seq 232 255)
         set bg (printf "48;5;%dm" $c)
-        printf " \033[%s %3s \033[0m" $bg $c
+        printf " \e[%s %3s \e[0m" $bg $c
         [ "$c" = '243' ]; and echo
     end
     echo -e "\n"
@@ -87,9 +89,9 @@ function colors::aixterm
     set text 'gYw'
 
     echo
-    echo -e -n "\033[1mHigh Intensity Colors (aixterm)\033[0m :\n"
-    echo -e -n "  foreground: ESC[{\033[38;5;196m90-97\033[0m}m\n"
-    echo -e -n "  background: ESC[{\033[38;5;196m100-107\033[0m}m\n"
+    echo -e -n "\e[1mHigh Intensity Colors (aixterm)\e[0m :\n"
+    echo -e -n "  foreground: ESC[{\e[38;5;196m90-97\e[0m}m\n"
+    echo -e -n "  background: ESC[{\e[38;5;196m100-107\e[0m}m\n"
 
     echo -e -n "\n     "
     for BG in '    ' '100m' '101m' '102m' '103m' '104m' '105m' '106m' '107m'
@@ -101,9 +103,9 @@ function colors::aixterm
 
     for i in (seq (count $foregrounds))
         set FG  $foregrounds[$i]
-        printf " %5s \033[$FG  $text " $FG
+        printf " %5s \e[$FG  $text " $FG
         for BG in '100m' '101m' '102m' '103m' '104m' '105m' '106m' '107m';
-            printf " \033[$FG\033[$BG  $text  \033[0m";
+            printf " \e[$FG\e[$BG  $text  \e[0m";
         end
         echo
     end
